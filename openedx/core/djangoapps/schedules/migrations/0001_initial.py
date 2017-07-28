b67731ee0b6b75e0fde49b2ccc964821560d591f
Clinton Blackburn <cblackburn@edx.org>
2017-07-28 15:38:15 -0400

    Created Schedule model
    This model will be used to control dynamic upgrade deadlines for
    learners.

---
+++ openedx/core/djangoapps/schedules/migrations/0001_initial.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import django_extensions.db.fields
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('student', '0010_auto_20170207_0458'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Schedule',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('modified', django_extensions.db.fields.ModificationDateTimeField(auto_now=True, verbose_name='modified')),
+                ('active', models.BooleanField(default=True, help_text='Indicates if this schedule is actively enforced')),
+                ('start', models.DateTimeField(help_text='Date this schedule went into effect')),
+                ('upgrade_deadline', models.DateTimeField(help_text='Deadline by which the learner must upgrade to a verified seat')),
+                ('enrollment', models.OneToOneField(to='student.CourseEnrollment')),
+            ],
+            options={
+                'verbose_name': 'Schedule',
+                'verbose_name_plural': 'Schedules',
+            },
+        ),
+    ]
---
+++ openedx/core/djangoapps/schedules/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.utils.translation import ugettext_lazy as _
+from django_extensions.db.fields import ModificationDateTimeField
+
+
+class Schedule(models.Model):
+    modified = ModificationDateTimeField(_('modified'))
+    enrollment = models.OneToOneField('student.CourseEnrollment', null=False)
+    active = models.BooleanField(default=True, help_text=_('Indicates if this schedule is actively enforced'))
+    start = models.DateTimeField(help_text=_('Date this schedule went into effect'))
+    upgrade_deadline = models.DateTimeField(
+        null=False,
+        help_text=_('Deadline by which the learner must upgrade to a verified seat')
+    )
+
+    class Meta(object):
+        verbose_name = _('Schedule')
+        verbose_name_plural = _('Schedules')
